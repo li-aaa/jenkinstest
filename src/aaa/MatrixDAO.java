60c12993365eb55367f83c1f6c1603b6371742fe
lxy <2424335402@qq.com>
2020-11-28 14:11:10 +0800

    del

--- src/aaa/MatrixDAO.java
+++
@@ -1,6 +0,0 @@
-package aaa;
-import org.apache.commons.math3.linear.Array2DRowRealMatrix;
-import org.apache.commons.math3.linear.RealMatrix;
-public interface MatrixDAO {
-	RealMatrix getFromDB(int mid);
-}
